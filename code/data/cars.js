6c5f6065f1784ce29049dc7c302132083fee0f1d
goaldae <goaldae@naver.com>
2019-06-11 13:47:30 +0900

    Add cars data as json

---
+++ code/data/cars.js
@@ -0,0 +1,90 @@
+module.exports =[
+    {
+        carName: "아반떼",
+        represent : 
+        {
+          engine: 1.6,
+          mileage: 14.5
+        },
+        fuel: {
+            gasoline: [
+                {
+                    engine: 1.6,
+                    mileage: 14.5
+                },
+                {
+                    engine: 2.2,
+                    mileage: 14.6
+                },
+                {
+                    engine: 3.0,
+                    mileage: 13.4
+                },
+            ],
+            diesel: [
+                {
+                    engine: 1.6,
+                    mileage: 14.5
+                },
+                {
+                    engine: 2.2,
+                    mileage: 14.6
+                },
+                {
+                    engine: 3.0,
+                    mileage: 13.4
+                },
+            ],
+            LPi: [
+                {
+                    engine: 2.4,
+                    mileage: 12.6
+                },                
+                {
+                    engine: 3.0,
+                    mileage: 9.2
+                },
+            ],
+        },
+    },
+    {
+        carName: "그랜저",
+        represent : 
+        {
+          engine: 3.0,
+          mileage: 14.5
+        },
+        fuel: {
+            gasoline: [
+                {
+                    engine: 3.0,
+                    mileage: 14.5
+                },
+                {
+                    engine: 2.4,
+                    mileage: 14.6
+                },
+            ],
+            diesel: [
+                {
+                    engine: 3.0,
+                    mileage: 17.2
+                },
+                {
+                    engine: 2.7,
+                    mileage: 15.9
+                },
+            ],
+            LPi: [
+                {
+                    engine: 3.0,
+                    mileage: 8.2
+                },                
+                {
+                    engine: 2.7,
+                    mileage: 11.3
+                },
+            ],
+        },
+    },
+]
