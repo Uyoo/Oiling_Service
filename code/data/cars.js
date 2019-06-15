ef58619097ac8b68c48348396e44377ff7393392
goaldae <goaldae@naver.com>
2019-06-15 15:51:30 +0900

    Add cars information with sunghoon

--- code/data/cars.js
+++ code/data/cars.js
@@ -1,90 +1,693 @@
 module.exports =[
+  {//아반떼,쏘나타,그랜저,K5,카니발, K9, 쏘렌토,스포티지,레이,모닝,싼타페
+    carName: "아반떼",
+    modelYear: 2019,
+    represent :
     {
-        carName: "아반떼",
-        represent : 
+      engine: 1.6,
+      mileage: 14.0
+    },
+    fuel: {
+      gasoline: [
         {
           engine: 1.6,
-          mileage: 14.5
-        },
-        fuel: {
-            gasoline: [
-                {
-                    engine: 1.6,
-                    mileage: 14.5
-                },
-                {
-                    engine: 2.2,
-                    mileage: 14.6
-                },
-                {
-                    engine: 3.0,
-                    mileage: 13.4
-                },
-            ],
-            diesel: [
-                {
-                    engine: 1.6,
-                    mileage: 14.5
-                },
-                {
-                    engine: 2.2,
-                    mileage: 14.6
-                },
-                {
-                    engine: 3.0,
-                    mileage: 13.4
-                },
-            ],
-            LPi: [
-                {
-                    engine: 2.4,
-                    mileage: 12.6
-                },                
-                {
-                    engine: 3.0,
-                    mileage: 9.2
-                },
-            ],
-        },
-    },
+          mileage: 14.0
+        }
+
+      ],
+      diesel: [
+        {
+          engine: 1.6,
+          mileage: 17.8
+        }
+      ],
+      LPi: [
+        {
+          engine: 1.6,
+          mileage: 10.6
+        }
+      ]
+    }
+  },
+  {
+    carName: "아반떼",
+    modelYear: 2018,
+    represent :
     {
-        carName: "그랜저",
-        represent : 
+      engine: 1.6,
+      mileage: 14.0
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 1.6,
+          mileage: 14.0
+        }
+
+      ],
+      diesel: [
+        {
+          engine: 1.6,
+          mileage: 17.8
+        }
+      ],
+      LPi: [
+        {
+          engine: 1.6,
+          mileage: 10.6
+        }
+      ]
+    }
+  },
+  
+  {
+    carName: "쏘나타",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 11.6
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 11.6
+        }
+      ],
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 15.6
+        }
+      ]
+    }
+  },
+  
+    //정확하지 않음 다시 해보기
+  {
+    carName: "쏘나타",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 12.0
+    },
+    fuel: {
+      : [
+        {
+          engine: 2.0,
+          mileage: 12.0
+        }
+      ],
+
+      GDiT: [
+        {
+          engine: 1.6,
+          mileage: 12.7
+        },
+        {
+          engine: 2.0,
+          mileage: 10.8
+        }
+      ],
+      eVGT: [
+        {
+          engine: 1.7,
+          mileage: 16.5
+        }
+      ],
+      LPi: [
+        {
+          engine: 2.0,
+          mileage: 9.6
+        }
+      ]
+    }
+  },
+  {
+    carName: "그랜저",
+    modelYear: 2019,
+    represent :
+    {
+      engine: 2.4,
+      mileage: 11.0
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.4,
+          mileage: 11.0
+        },
         {
           engine: 3.0,
-          mileage: 14.5
+          mileage: 9.9
         },
-        fuel: {
-            gasoline: [
-                {
-                    engine: 3.0,
-                    mileage: 14.5
-                },
-                {
-                    engine: 2.4,
-                    mileage: 14.6
-                },
-            ],
-            diesel: [
-                {
-                    engine: 3.0,
-                    mileage: 17.2
-                },
-                {
-                    engine: 2.7,
-                    mileage: 15.9
-                },
-            ],
-            LPi: [
-                {
-                    engine: 3.0,
-                    mileage: 8.2
-                },                
-                {
-                    engine: 2.7,
-                    mileage: 11.3
-                },
-            ],
-        },
+        {
+          engine: 3.3,
+          mileage: 9.7
+        }
+      ],
+
+      LPi: [
+        {
+          engine: 3.0,
+          mileage: 7.4
+        }
+      ]
+    }
+  },
+  {
+    carName: "그랜저",
+    modelYear: 2018, 
+    represent :
+    {
+      engine: 2.4,
+      mileage: 11.0
     },
-]
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.4,
+          mileage: 11.0
+        },
+        {
+          engine: 3.0,
+          mileage: 9.9
+        },
+        {
+          engine: 3.3,
+          mileage: 9.7
+        }
+      ],
+
+      LPi: [
+        {
+          engine: 3.0,
+          mileage: 7.4
+        }
+      ],
+
+      diesel: [
+        {
+          engine: 2.2,
+          mileage: 14.3
+        }
+      ]
+    }
+  },
+  {
+    carName: "그랜저",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.4,
+      mileage: 11.0
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.4,
+          mileage: 11.0
+        },
+        {
+          engine: 3.0,
+          mileage: 9.9
+        },
+        {
+          engine: 3.3,
+          mileage: 9.7
+        }
+      ],
+
+      LPi: [
+        {
+          engine: 3.0,
+          mileage: 7.4
+        }
+      ],
+
+      diesel: [
+        {
+          engine: 2.2,
+          mileage: 14.3
+        }
+      ]
+    }
+  },
+  {
+    carName: "K5",
+    modelYear: 2020,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 11.6
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 11.6
+        }
+      ],
+
+      LPi: [
+        {
+          engine: 2.0,
+          mileage: 9.4
+        }
+      ],
+
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 15.6
+        }
+      ]
+    }
+  },
+  {
+    carName: "K5",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 11.6
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 11.6
+        }
+      ],
+      
+      LPi: [
+        {
+          engine: 2.0,
+          mileage: 9.4
+        }
+      ],
+
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 15.6
+        }
+      ]
+    }
+  },
+
+  {
+    carName: "K5",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 11.6
+    },
+    fuel: {
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 16.1
+        }
+      ]
+    }
+  },
+
+  {//이건 어떻게 할지
+    carName: "더뉴 카니발",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.2,
+      mileage: 11.3
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.2,
+          mileage: 11.3
+        },
+        {
+          engine: 3.3,
+          mileage: 8.2
+        }
+      ]
+    }
+  },
+
+ 
+  {
+    carName: "카니발",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.2,
+      mileage: 10.9
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.2,
+          mileage: 10.9
+        },
+        {
+          engine: 3.3,
+          mileage: 7.9
+        }
+      ]
+    }
+  },
+  {
+    carName: "카니발",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.2,
+      mileage: 11.2
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.2,
+          mileage: 11.2
+        },
+        {
+          engine: 3.3,
+          mileage: 8.3
+        }
+      ]
+    }
+  },
+  {
+    carName: "K9",
+    modelYear: 2020,
+    represent :
+    {
+      engine: 3.8,
+      mileage: 8.8
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 3.8,
+          mileage: 8.8
+        },
+        {
+          engine: 5.0,
+          mileage: 7.5
+        }
+      ],
+    }
+  },
+
+  {
+    carName: "K9",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 3.3,
+      mileage: 8.6
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 3.3,
+          mileage: 8.6
+        },
+        {
+          engine: 3.8,
+          mileage: 8.4
+        },
+        {
+          engine: 5.0,
+          mileage: 7.2
+        }
+      ]
+    }
+  },
+  {
+    carName: "쏘렌토",
+    modelYear: 2020,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 9.4
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 9.4
+        }
+      ],
+      diesel: [
+        {
+          engine: 2.0,
+          mileage: 13.1
+        },
+        {
+          engine: 2.2,
+          mileage: 13.2
+        }
+      ],
+      LPi: [
+        {
+          engine: 1.6,
+          mileage: 10.6
+        }
+      ]
+    }
+  },
+  {
+    carName: "스포티지",
+    modelYear: 2019,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 10.5
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 10.5
+        }
+      ],
+      diesel: [
+        {
+          engine: 1.6,
+          mileage: 15.3
+        },
+        {
+          engine: 2.0,
+          mileage: 13.8
+        }
+      ]
+    }
+  },
+  {
+    carName: "스포티지",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 10.3
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 10.3
+        }
+      ],
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 14.6
+        },
+        {
+          engine: 2.0,
+          mileage: 12.0
+        }
+      ]
+    }
+  },
+  {
+    carName: "스포티지",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 10.3
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 2.0,
+          mileage: 10.3
+        }
+      ],
+      diesel: [
+        {
+          engine: 1.7,
+          mileage: 14.6
+        },
+        {
+          engine: 2.0,
+          mileage: 12.0
+        }
+      ]
+    }
+  },
+  {
+    carName: "레이",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 1.0,
+      mileage: 12.7
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 1.0,
+          mileage: 12.7
+        }
+      ],
+      LPi: [
+        {
+          engine: 1.0,
+          mileage: 10.5
+        }
+      ]
+    }
+  },
+  {
+    carName: "모닝",
+    modelYear: 2019,
+    represent :
+    {
+      engine: 1.0,
+      mileage: 14.7
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 1.0,
+          mileage: 14.7
+        }
+      ],
+      LPi: [
+        {
+          engine: 1.0,
+          mileage: 11.8
+        }
+      ]
+    }
+  },
+  
+  {
+    carName: "싼타페",
+    modelYear: 2019,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 9.5
+    },
+    fuel: {
+      gasolineT: [
+        {
+          engine: 2.0,
+          mileage: 9.5
+        }
+
+      ],
+      diesel: [
+        {
+          engine: 2.0,
+          mileage: 13.1
+        },
+        {
+          engine: 2.2,
+          mileage: 12.8
+        }
+      ]
+    }
+  },
+  {
+    carName: "싼타페",
+    modelYear: 2018,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 9.0
+    },
+    fuel: {
+      gasolineT: [
+        {
+          engine: 2.0,
+          mileage: 9.0
+        }
+
+      ],
+      diesel: [
+        {
+          engine: 2.0,
+          mileage: 12.0
+        },
+        {
+          engine: 2.2,
+          mileage: 12.0
+        }
+      ]
+    }
+  },
+  {
+    carName: "싼타페",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 2.0,
+      mileage: 9.0
+    },
+    fuel: {
+      diesel: [
+        {
+          engine: 2.0,
+          mileage: 13.1
+        },
+        {
+          engine: 2.2,
+          mileage: 12.8
+        }
+      ]
+    }
+  },
+ 
+  {
+    carName: "엑센트",
+    modelYear: 2017,
+    represent :
+    {
+      engine: 1.4,
+      mileage: 13.4
+    },
+    fuel: {
+      gasoline: [
+        {
+          engine: 1.4,
+          mileage: 13.4
+        }
+      ],
+      diesel: [
+        {
+          engine: 1.6,
+          mileage: 17.6
+        }
+      ]
+    }
+  }
+ ]
